fbc4734624f825f33df4618f39341df966a7045d
Kevin Velasco <me@kvnvelasco.com>
2024-12-03 11:51:50 +1100

    better damping logic

--- day_02/lib/dampener.ex
+++ day_02/lib/dampener.ex
@@ -9,12 +9,12 @@ defmodule ProblemDampener do
     # So the hack here is that we go from negative index to the end.
     # Lists in elixir are all linked lists so it's more efficient to
     # start our search at the front which would be (-length)
-    safety_dampener(level, -(length(level) - 1))
+    safety_dampener(level, 0..length(level))
   end
 
-  def safety_dampener(level, damped_item) when damped_item <= 0 do
-    case SafeLevels.is_level_safe(List.delete_at(level, damped_item)) do
-      false -> safety_dampener(level, damped_item + 1)
+  def safety_dampener(level, index..limit) when index != limit do
+    case SafeLevels.is_level_safe(List.delete_at(level, index)) do
+      false -> safety_dampener(level, (index + 1)..limit)
       true -> true
     end
   end
